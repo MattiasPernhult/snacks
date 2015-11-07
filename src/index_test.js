7f2a9724c3101e24017dc3341d1d0a3cd1dc166a
Mattias Pernhult <pernhultmattias@gmail.com>
2015-11-07 15:32:47 +0100

    Added tests to cover the code

---
+++ src/index_test.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect;
+var snacks = require('./index');
+
+describe('snacks', function() {
+    describe('all', function() {
+        it('should be an array of object strings', function() {
+            expect(snacks.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(arr) {
+                return arr.every(function(item) {
+                    return typeof item.name === 'string' &&
+                        typeof item.kind === 'string' &&
+                        typeof item.brand === 'string';
+                });
+            }
+        });
+
+        it('should include `Sourcream and Onion`', function() {
+            expect(snacks.all[0].name).to.include('Sourcream and Onion');
+        });
+    });
+
+    describe('random', function() {
+        it('should return a random item from snacks.all', function() {
+            var randomItem = snacks.random();
+            expect(snacks.all).to.include(randomItem);
+        });
+    });
+});
